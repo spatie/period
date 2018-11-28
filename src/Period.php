2c0a06ddd8928b31861d51e3e259b359811f346f
freek <freek@spatie.be>
2018-11-28 10:23:09 +0100

    fix docblock

--- src/Period.php
+++ src/Period.php
@@ -26,8 +26,8 @@ class Period
     }
 
     /**
-     * @param $start
-     * @param $end
+     * @param \DateTime|string $start
+     * @param \DateTime|string $end
      * @param string|null $format
      *
      * @return \Spatie\Period\Period|static
