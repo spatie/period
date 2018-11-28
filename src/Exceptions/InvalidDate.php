04ef115851edf2796c93e880e1f4833ad63d8133
freek <freek@spatie.be>
2018-11-28 10:20:08 +0100

    still fits on one line

--- src/Exceptions/InvalidDate.php
+++ src/Exceptions/InvalidDate.php
@@ -7,10 +7,8 @@ use Spatie\Period\InvalidDate;
 
 class InvalidDate extends InvalidArgumentException
 {
-    public static function forFormat(
-        string $date,
-        string $format
-    ): InvalidDate {
+    public static function forFormat(string $date, string $format): InvalidDate
+    {
         return new static("Could not construct a date from `{$date}` with format `{$format}`.");
     }
 }
--- src/Exceptions/InvalidPeriod.php
+++ src/Exceptions/InvalidPeriod.php
@@ -7,10 +7,8 @@ use InvalidArgumentException;
 
 class InvalidPeriod extends InvalidArgumentException
 {
-    public static function endBeforeStart(
-        DateTimeImmutable $start,
-        DateTimeImmutable $end
-    ): InvalidPeriod {
+    public static function endBeforeStart(DateTimeImmutable $start, DateTimeImmutable $end): InvalidPeriod
+    {
         return new static("The end time `{$end}` is before the start time `{$start}`.");
     }
 }
