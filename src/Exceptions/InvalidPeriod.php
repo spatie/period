8e00727275234bbb3392564b4a1ca5ab552752c6
Brent Roose <brent@spatie.be>
2018-11-28 11:17:24 +0100

    Fix invalid period exception message

--- src/Exceptions/InvalidPeriod.php
+++ src/Exceptions/InvalidPeriod.php
@@ -9,6 +9,6 @@ class InvalidPeriod extends InvalidArgumentException
 {
     public static function endBeforeStart(DateTimeImmutable $start, DateTimeImmutable $end): InvalidPeriod
     {
-        return new static("The end time `{$end}` is before the start time `{$start}`.");
+        return new static("The end time `{$end->format('Y-m-d H:i:s')}` is before the start time `{$start->format('Y-m-d H:i:s')}`.");
     }
 }
