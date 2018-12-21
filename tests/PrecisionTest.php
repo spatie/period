44d967f6f5102041e32f559128338fa265e7c462
Brent Roose <brent@spatie.be>
2018-12-21 09:05:10 +0100

    Add some more tests

--- tests/PrecisionTest.php
+++ tests/PrecisionTest.php
@@ -58,4 +58,70 @@ class PrecisionTest extends TestCase
 
         $a->overlapsWith($b);
     }
+
+    /** @test */
+    public function precision_with_seconds()
+    {
+        $a = Period::make('2018-01-01 00:00:15', '2018-01-01 00:00:15', Precision::SECOND);
+        $b = Period::make('2018-01-01 00:00:15', '2018-01-01 00:00:15', Precision::SECOND);
+        $c = Period::make('2018-01-01 00:00:16', '2018-01-01 00:00:16', Precision::SECOND);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
+
+    /** @test */
+    public function precision_with_minutes()
+    {
+        $a = Period::make('2018-01-01 00:15:00', '2018-01-01 00:15:00', Precision::MINUTE);
+        $b = Period::make('2018-01-01 00:15:00', '2018-01-01 00:15:00', Precision::MINUTE);
+        $c = Period::make('2018-01-01 00:16:00', '2018-01-01 00:16:00', Precision::MINUTE);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
+
+    /** @test */
+    public function precision_with_hours()
+    {
+        $a = Period::make('2018-01-01 15:00:00', '2018-01-01 15:00:00', Precision::HOUR);
+        $b = Period::make('2018-01-01 15:00:00', '2018-01-01 15:00:00', Precision::HOUR);
+        $c = Period::make('2018-01-01 16:00:00', '2018-01-01 16:00:00', Precision::HOUR);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
+
+    /** @test */
+    public function precision_with_days()
+    {
+        $a = Period::make('2018-01-01', '2018-01-01', Precision::DAY);
+        $b = Period::make('2018-01-01', '2018-01-01', Precision::DAY);
+        $c = Period::make('2018-01-02', '2018-01-02', Precision::DAY);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
+
+    /** @test */
+    public function precision_with_months()
+    {
+        $a = Period::make('2018-01-01', '2018-01-01', Precision::MONTH);
+        $b = Period::make('2018-01-01', '2018-01-01', Precision::MONTH);
+        $c = Period::make('2018-02-01', '2018-02-01', Precision::MONTH);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
+
+    /** @test */
+    public function precision_with_years()
+    {
+        $a = Period::make('2018-01-01', '2018-01-01', Precision::YEAR);
+        $b = Period::make('2018-01-01', '2018-01-01', Precision::YEAR);
+        $c = Period::make('2019-01-01', '2019-01-01', Precision::YEAR);
+
+        $this->assertTrue($a->overlapsWith($b));
+        $this->assertFalse($a->overlapsWith($c));
+    }
 }
